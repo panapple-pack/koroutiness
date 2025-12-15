77b75d52104bd406b13dcf20c6ad51ebd5ea5f1e
panapple-pack <199948304+panapple-pack@users.noreply.github.com>
2025-12-15 18:25:22 +0300

    lesson8.task

--- src/main/kotlin/lesson8/GameLoop.kt
+++ src/main/kotlin/lesson8/GameLoop.kt
@@ -1,30 +1,41 @@
 package lesson8
 
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 fun main() {
     println("=== Простая имитация игрового времени и цикла ===")
 
     val gameTime = GameTime()
+    val enemies: MutableList<Enemy> = mutableListOf()
 
     val player = Player(
         x = 0.0,
         speed = 2.0,
-        name = "Oleg"
+        name = "Oleg",
+        maxHealth = 100,
     )
 
-    val enemy = Enemy(
-        x = 10.0,
-        speed = -1.0,   // Скорость -1.0, т.к враг двигается влево (к игроку)
-        id = 1
-    )
+    for (i in Random.nextInt(2,4) - 1 .. 4) {
+        val enemy = Enemy(
+            x = Random.nextDouble(25.0, 101.0),
+            speed = 0.0,   // Скорость -1.0, т.к враг двигается влево (к игроку)
+            id = i,
+        )
+        enemies.add(enemy)
+    }
+
 
     println("Начальное положение сущность ")
     player.printPosition()
-    enemy.printPosition()
+    for (enemy in enemies) {
+        enemy.printPosition()
+    }
 
     println()
     println("Начало игры")
 
-    while (true) {
+    while (player.isAlive || player.x <= 110) {
         gameTime.update()
 
         val dt = gameTime.deltaTimeSeconds
@@ -33,7 +44,6 @@ fun main() {
 
         // обновляем позиции объектов по времени
         player.update(dt)
-        enemy.update(dt)
 
         // Выводим информацию
         println()
@@ -42,28 +52,38 @@ fun main() {
         // %.3f - число с 3 знаками после запятой (например 1.234)
 
         player.printPosition()
-        enemy.printPosition()
-
-        // Проверим, "столкнулись" ли игрок и враг
-        val distance = enemy.x - player.x
-        // distance - расстояние между врагом и игроком по оси Х
-        // enemy.x - player.x - обычное вычитание двух Double
-
-        println("Расстояние между игроком и врагом: ${"%.3f".format(distance)}")
-
-        if (distance <= 0.0) {
-            // Если расстояние <= 0, считаем, что они встретились или пересеклись
-            println()
-            println("Игрок и враг столкнулись! Останавливаем игровой цикл.")
-            break
-            // break - выходим из цикла while (true)
+        for (enemy in enemies) {
+            enemy.printPosition()
         }
 
-        // Немного "спим", чтобы цикл не крутился слишком быстро
-        Thread.sleep(200)
-        // Thread - класс, представляющий поток (здесь главный поток программы)
-        // .sleep(200) - приостановить поток на 200 миллисекунд
-        // Это нужно только для того, чтобы человек успевал видеть изменения в консоли
+        // Проверим, "столкнулись" ли игрок и враг
+        for (enemy in enemies) {
+            val distance = enemy.x - player.x
+            // distance - расстояние между врагом и игроком по оси Х
+            // enemy.x - player.x - обычное вычитание двух Double
+
+            println("Расстояние между игроком и врагом: ${"%.3f".format(distance)}")
+
+            if (distance <= 0.0) {
+                // Если расстояние <= 0, считаем, что они встретились или пересеклись
+                println()
+                println("Игрок и враг столкнулись!")
+                while (!player.isAlive || !enemy.isAlive) {
+                    player.attack(enemy)
+                    enemy.attack(player)
+                }
+            }
+            if (!player.isAlive) {
+                break
+            }
+
+            // Немного "спим", чтобы цикл не крутился слишком быстро
+            Thread.sleep(200)
+            // Thread - класс, представляющий поток (здесь главный поток программы)
+            // .sleep(200) - приостановить поток на 200 миллисекунд
+            // Это нужно только для того, чтобы человек успевал видеть изменения в консоли
+        }
+
     }
 
     println()
--- src/main/kotlin/lesson8/GameObject.kt
+++ src/main/kotlin/lesson8/GameObject.kt
@@ -1,5 +1,8 @@
 package lesson8
 
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 open class GameObject(
     // open - ключевое слово, которое позволяет наследовать данный класс другим классам
 
@@ -21,23 +24,47 @@ open class GameObject(
 }
 
 class Player(
+    var isAlive: Boolean = true,
     x: Double,
     speed: Double,
-    val name: String
+    val name: String,
+    var damage: Int = Random.nextInt(10, 41),
+    var maxHealth: Int
 ) : GameObject(x, speed) {
     // GameObject означает наследовать родителя с передачей параметров базовому классу
     fun printPosition() {
         println("Игрок $name находится сейчас на позиции x = $x")
     }
+
+    fun attack(target: Enemy) {
+        target.maxHealth -= damage
+        println("Игрок нанес врагу $damage урона. Здоровье врага: ${target.maxHealth}")
+        if (target.maxHealth <= 0) {
+            target.isAlive = false
+            println("Игрок побеждает вражину")
+        }
+    }
 }
 
 class Enemy(
+    var isAlive: Boolean = true,
     x: Double,
     speed: Double,
-    val id: Int
+    val id: Int,
+    var damage: Int = Random.nextInt(5, 21),
+    var maxHealth: Int = Random.nextInt(40, 91)
 ) : GameObject(x, speed) {
     fun printPosition() {
         println("Враг $id находится сейчас на позиции x = $x")
+    }
+
+    fun attack(target: Player) {
+        target.maxHealth -= damage
+        println("Враг нанес игроку $damage урона. Здоровье игрока: ${target.maxHealth}")
+        if (target.maxHealth <= 0) {
+            target.isAlive = false
+            println("Игрок погиб")
+        }
     }
 }
 
